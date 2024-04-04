6950037e1ee00bfe823861407111faae9ab8c220
Logon Aluno <logonrmlocal@fiap.com.br>
2024-04-03 22:43:36 -0300

    Javascript 1

---
+++ script.js
@@ -0,0 +1,9 @@
+// alert('Olá, mundo')
+// prompt('Digite o seu nome')
+// confirm('Você deseja sair')
+
+var nome = prompt('Digite o seu nome')
+var idade = prompt('Digite a sua idade')
+var profissão = prompt('Digite a sua profissão')
+
+console.log(`Olá ${nome}, você tem ${idade} anos e trabalha como ${profissão}`)
